403226328bb2cb729cc037953f37e310d259ddb5
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-07-01 20:00:35 +0530

    Add files via upload

---
+++ Anagram.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+def main():
+    word = input("Enter word : ")
+    word2 = input("Enter Anagram : ")
+    s = list(word2)
+    for i in word:
+        if i in s:
+            s.remove(i)
+            continue
+        
+    if(s == []):
+        print("Is Anagram")
+    else:
+        print("Not Anagram")
+
+if __name__ == "__main__":
+    main()
+    
