c4aa125e35df61df8c30fcea8da52e5f4bf14d03
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-07-02 19:35:25 +0530

    Add files via upload

---
+++ Average Salary Excluding the Minimum and Maximum Salary.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+def main():
+
+    salary = list(map(int,input("Enter the salary : ").strip().split()))
+    n = len(salary)
+    total = 0
+
+    for i in range(n):
+        for j in range(i+1, n):
+            if (salary[i] < salary[j]):
+                salary[i], salary[j] = salary[j], salary[i]
+
+    for i in range(1, n-1):
+        total +=salary[i]
+
+    average = total // (n-2)
+
+    print(average)
+        
+if __name__ == "__main__":
+    main()
