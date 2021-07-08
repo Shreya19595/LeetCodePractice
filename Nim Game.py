d2e4a6418c213f3cc81fc6486a196e8c035b2f7f
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-07-08 17:30:51 +0530

    Add files via upload

---
+++ Nim Game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def main():
+    n = int(input("Enter number of Stones : "))
+
+    if (n % 4 == 0):
+        print("False")
+    else:
+        print("True")
+
+if __name__ == "__main__":
+    main()
