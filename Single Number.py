84eb805d4159228ea005c959437908be02b65eba
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-07-12 18:00:49 +0530

    Update Single Number.py

--- Single Number.py
+++ Single Number.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+'''
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+'''
+
 def main():
     nums = list(map(int,input("Enter the array : ").strip().split()))
     n = len(nums)
