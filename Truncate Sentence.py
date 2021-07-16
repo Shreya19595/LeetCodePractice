c0a885264689f01310bb4b42fc0bb314b563fc60
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-07-16 20:45:07 +0530

    Update Truncate Sentence.py

--- Truncate Sentence.py
+++ Truncate Sentence.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+'''
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. 
+Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+You are given a sentence s and an integer k. You want to truncate s such that it contains only the first k words. Return s after truncating it.
+'''
+
 def main():
     sentence = input("Enter the Sentence : ")
     k = int(input("Enter number of words to be truncated : ")
