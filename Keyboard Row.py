78aa31bf503f7e8105b1f5fce94cf1d0e589f341
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-07-26 19:24:19 +0530

    Update Keyboard Row.py

--- Keyboard Row.py
+++ Keyboard Row.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
+'''
+Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard like the image below.
+In the American keyboard:
+the first row consists of the characters "qwertyuiop",
+the second row consists of the characters "asdfghjkl", and
+the third row consists of the characters "zxcvbnm".
+'''
 
 def main():
     words = list(input("Enter words : ").strip().split())
