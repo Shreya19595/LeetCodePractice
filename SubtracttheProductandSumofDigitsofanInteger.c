baf6ce413edfc4be76b8f910d60e6617e14d8008
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-09-04 19:46:19 +0530

    Add files via upload

---
+++ SubtracttheProductandSumofDigitsofanInteger.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*Given an integer number n, return the difference between the product of its digits and the sum of its digits.*/
+
+int main()
+{
+    int n;
+    scanf("Enter number: %d\n, n);
+    int prod, sum, result;
+    sum = 0;
+    prod = 1;
+    while(n > 0) {
+        int a = n % 10;
+        sum += a;
+        prod *= a;
+        n /= 10;
+    } 
+    result = prod - sum;
+    printf("%d\n", result);
+    return 0;
+}
