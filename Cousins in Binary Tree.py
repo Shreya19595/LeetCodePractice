86c2e8f454b8f9f39969b103a789f3d659625bc3
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-10-18 23:04:10 +0530

    Add files via upload

---
+++ Cousins in Binary Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        levels = {}
+        def traverse(node, parent , level):
+            if not node:
+                return
+            levels[level] = levels.get(level,{})
+            levels[level].update({node.val:parent})
+            traverse(node.left, node.val, level + 1)
+            traverse(node.right, node.val, level + 1)
+            
+        traverse(root,None,0)
+        
+        for l in levels:
+            if x in levels[l] and y in levels[l]:
+                if levels[l][x] != levels[l][y]:
+                    return True
+        return False
