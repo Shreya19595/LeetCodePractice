908d10619484a6e71a24f5466c162ce2f0c07d8d
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-11-04 19:20:49 +0530

    Add files via upload

---
+++ Sum of Left Leaves.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+'''
+Given the root of a binary tree, return the sum of all left leaves.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+
+        else:
+            return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
