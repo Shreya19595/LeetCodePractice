365738494ca73aaa5ad085ba91bc3d4e7044f567
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-12-04 12:55:11 +0530

    Add files via upload

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -2,24 +2,23 @@
 
 '''
 Write a function to find the longest common prefix string amongst an array of strings.
-If there is no common prefix, return not found.
+If there is no common prefix, return an empty string "".
 '''
 
 def main():
+    strs = list(input("Enter the array: ").strip().split())
 
-    strs = []
-    strs = [item for item in input("Enter the words : ").split()]
-    def commonword(strs):
-        common = []
-        for l in strs[0]:
-            if all(s.startswith("".join(common)+l) for s in strs): # iterates over every word in srs array and checks if they have common prefix
-                common.append(l)
-            else:
-                break
-        return "".join(common)
-
-    prefix = commonword(strs)
-    print(prefix)
+    strs.sort()
     
+    output = ""
+    
+    for i in range(len(strs[0])):
+        if strs[0][i] == strs[len(strs)-1][i]:
+            output += strs[0][i]
+        else:
+            break
+    
+    print(output)
+
 if __name__ == "__main__":
     main()
