d23ca49cbd69586458071a89f87b38124a1dc968
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-12-08 12:30:33 +0530

    Add files via upload

---
+++ Binary Tree Tilt.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+'''
+Given the root of a binary tree, return the sum of every tree node's tilt.
+The tilt of a tree node is the absolute difference between the sum of all left subtree node values and all right subtree node values. If a node does not have a left child, then the sum of the left subtree node values is treated as 0. The rule is similar if there the node does not have a right child.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        
+        def helper(root):
+            if not root: 
+                return 0
+            
+            left = helper(root.left)
+            right = helper(root.right)
+            self.ans += abs(left - right)
+            
+            return root.val + left + right
+        
+        self.ans = 0
+        helper(root)
+        return self.ans
